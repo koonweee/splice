4778ef6423079f0757a503ea027c120d803b93f8
Jeremy Tan <jeremy.tan@newfront.com>
2025-06-22 15:27:56 -0700

    refactor: remove unused JwtPayload interface from request.ts

--- splice-api/src/common/request.ts
+++ splice-api/src/common/request.ts
@@ -1,10 +1,5 @@
 import type { User } from '../users';
 
-interface JwtPayload {
-  sub: string; // userId
-  email: string;
-}
-
 export interface AuthenticatedRequest extends Request {
   user: User;
 }
